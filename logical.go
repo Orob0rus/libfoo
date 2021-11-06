ff463ceeec7b8240396a582033c5f5276002cdb4
Vishnu Goyal <goyal.5.vishnu@gmail.com>
2021-11-06 23:59:13 +0530

    added Any function

--- logical.go
+++ logical.go
@@ -8,3 +8,16 @@ func And(vals ...bool) bool {
 	}
 	return true
 }
+
+func Any(function func(interface{}) (bool, error)) func([]interface{}) (bool, error) {
+	return func(vals []interface{}) (bool, error) {
+		for _, val := range vals {
+			if out, err := function(val); err != nil {
+				return false, err
+			} else if out {
+				return true, nil
+			}
+		}
+		return false, nil
+	}
+}
